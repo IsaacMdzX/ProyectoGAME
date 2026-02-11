38141eddfe3a2a881bd4da90175c4c27bfbf9489
Isaac Mendez <isaacmendeztp@gmail.com>
2026-02-10 22:09:10 -0600

    2

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app = create_app()
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Esto crea la app usando el factory y la ejecuta cuando se ejecuta este módulo directamente
+    app.run(host='0.0.0.0', port=5000, debug=True)
